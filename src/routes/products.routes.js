8a62f9bee509f336d36a07d2e6194ba0dcb1775e
cristhian <cristhianbrh@gmail.com>
2025-11-02 23:24:48 -0500

    Se agrega el put y delete al products.router.js

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,10 +1,18 @@
 import express from 'express';
-import { getProducts, getProduct, createNewProduct } from '../controllers/products.controller.js';
+import { 
+    getProducts, 
+    getProduct, 
+    createNewProduct,
+    updateProduct,
+    deleteProduct
+    } from '../controllers/products.controller.js';
 
 const router = express.Router();
 
 router.get('/', getProducts);
 router.get('/:id', getProduct);
 router.post('/', createNewProduct);
+router.put('/:id', updateProduct);
+router.delete('/:id', deleteProduct);
 
 export default router;
